4f76a2b04f88b9ac9284fc8f73c10bb56bbbcda5
jd <root@example.com>
2025-12-14 18:48:40 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # brick-breaker
+Ce jeu est une traduction du tutorial de Google pour apprendre Dart et Flutter.
+
+Chaque répertoire est une version incrémentale du jeu.
+
+Les étapes sont documentées dans les fichiers du répertoire ./doc.
+
---
+++ v3/lib/src/components/ball.dart
@@ -0,0 +1,25 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Ball extends CircleComponent {
+  Ball({
+    required this.velocity,
+    required super.position,
+    //required super.radius, # marche aussi
+    required radius,
+  }) : super(
+         radius: radius,
+         anchor: Anchor.center,
+         paint: Paint()
+           ..color = const Color(0xff1e6091)
+           ..style = PaintingStyle.fill,
+       );
+
+  final Vector2 velocity;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position += velocity * dt;
+  }
+}
